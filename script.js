7daff4fb4db277796014194c2c2b48242d89fdf6
bub3ns <Bubs0n.Company@gmail.com>
2026-02-04 13:45:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function playAudioAndVideo() {
+    var audio = document.getElementById('audio');
+    var video = document.getElementById('video');
+    video.volume = 0;
+    audio.volume = 0.3;
+
+    if (audio && video) {
+        setTimeout(function () {
+            audio.play();
+        }, 50);
+        video.play();
+    }
+}
+
+let titleText = "bub3ns.xyz   ";
+let titleArray = titleText.split("");
+let titleInterval = null;
+let started = false;
+
+function startTitleAnimation() {
+    if (started) return; // żeby nie odpalić 2x
+    started = true;
+
+    document.title = titleText;
+
+    titleInterval = setInterval(() => {
+        const first = titleArray.shift(); // zabierz pierwszą literę
+        titleArray.push(first);           // wrzuć ją na koniec
+        document.title = titleArray.join("");
+    }, 500);
+}
